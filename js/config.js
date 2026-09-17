e08b2ad0fe2383340560b005a592e523e0ee15f3
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-17 20:27:22 -0300

    Centraliza configuración de invitación Toy Story

---
+++ js/config.js
@@ -0,0 +1,47 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Thiago",
+    age: 2,
+    message: "Vení a celebrarlo junto a familia y amigos. ¡Nos divertiremos con muchos juegos y sorpresas!",
+    eventDateTime: "2026-12-08T20:00:00-03:00",
+    eventEndDateTime: "2026-12-08T22:30:00-03:00",
+    dateLabel: "Martes 8 de Diciembre",
+    timeLabel: "20:00 a 22:30 hs.",
+    venue: "Salón de Fiestas Cirilo",
+    address: "Av. Álvarez Thomas 1320",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=Sal%C3%B3n+de+Fiestas+Cirilo+Av.+%C3%81lvarez+Thomas+1320+Buenos+Aires",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Thiago. 🚀⭐",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio",
+    audioUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionToyStory/main/www.tarjetas.cool/modelos/cumple/toy_story/sounds/audio.mp3",
+    portraitUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionToyStory/main/www.tarjetas.cool/modelos/cumple/toy_story/img/foto1.png",
+    ribbonUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionToyStory/main/www.tarjetas.cool/modelos/cumple/toy_story/img/cinta.png"
+  },
+  characters: [
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionToyStory/main/www.tarjetas.cool/modelos/cumple/toy_story/img/1.png",
+      width: "min(118vw, 500px)",
+      top: "-20px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionToyStory/main/www.tarjetas.cool/modelos/cumple/toy_story/img/4.png",
+      width: "min(120vw, 510px)",
+      top: "-62px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionToyStory/main/www.tarjetas.cool/modelos/cumple/toy_story/img/3.png",
+      width: "min(108vw, 460px)",
+      top: "-20px",
+      translateX: "-48%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionToyStory/main/www.tarjetas.cool/modelos/cumple/toy_story/img/2.png",
+      width: "min(58vw, 245px)",
+      top: "-22px",
+      translateX: "-50%"
+    }
+  ]
+};
