c413ada7ff13f4ef81c823c96abb8e84c078e7d7
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 20:06:36 -0300

    Desactiva destino de confirmación en demo

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ window.INVITATION_CONFIG = {
     venue: "Salón de Fiestas Cirilo",
     address: "Av. Álvarez Thomas 1320",
     mapUrl: "https://www.google.com/maps/search/?api=1&query=Sal%C3%B3n+de+Fiestas+Cirilo+Av.+%C3%81lvarez+Thomas+1320+Buenos+Aires",
-    whatsappNumber: "5491156223007",
+    whatsappNumber: "",
     whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Thiago. 🚀⭐",
     instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
     instagramHandle: "@rossdigitalstudio",
